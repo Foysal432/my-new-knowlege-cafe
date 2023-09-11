e5e05dc42c1c19bb476948c9399655c263f9a792
foysal432 <foysalarham69@gmail.com>
2023-09-11 21:23:42 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+import Blogs from './componets/Blogs/Blogs'
+import Header from './componets/Header/Header'
+
+function App() {
+ 
+
+  return (
+    <>
+   <Header></Header>
+   <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
